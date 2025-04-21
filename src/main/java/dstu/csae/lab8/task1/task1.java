f2fa589f6193528930bb345912f870953e727b2d
SergeyYakushevskiy <yakushevskiy.s.s@mail.ru>
2025-04-22 00:13:54 +0300

    done lab8.task1

--- src/main/java/dstu/csae/lab8/task1/task1.java
+++ src/main/java/dstu/csae/lab8/task1/task1.java
@@ -3,7 +3,19 @@ package dstu.csae.lab8.task1;
 public class task1 {
 
     public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isKotlinBetter = false;
 
+        System.out.println("Java is fun? " + isJavaFun);
+        System.out.println("Kotlin is better? " + isKotlinBetter);
+
+        boolean andResult = isJavaFun & isKotlinBetter;
+        boolean orResult = isJavaFun | isKotlinBetter;
+        boolean notResult = !isJavaFun;
+
+        System.out.println("AND: " + andResult);
+        System.out.println("OR: " + orResult);
+        System.out.println("NOT: " + notResult);
     }
 
 }
--- src/main/java/dstu/csae/lab8/task1/task1.kt
+++ src/main/java/dstu/csae/lab8/task1/task1.kt
@@ -1,5 +1,17 @@
 package dstu.csae.lab8.task1
 
 fun main(){
-    println("Hello, worldw")
+    val isKotlinFun: Boolean = true
+    val isJavaOld: Boolean = false
+
+    println("Kotlin is fun? $isKotlinFun")
+    println("Java is old? $isJavaOld")
+
+    val andResult = isKotlinFun && isJavaOld
+    val orResult = isKotlinFun || isJavaOld
+    val notResult = !isKotlinFun
+
+    println("AND: $andResult")
+    println("OR: $orResult")
+    println("NOT: $notResult")
 }
